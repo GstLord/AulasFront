e2cbcaecd2555a2ccc38706ac1bfdcb20c83a287
GustavoCesar <gustavosommer123@gmail.com>
2026-04-08 17:53:15 -0300

    atualizando pastaFront

---
+++ AulasFrontEBAC/JavaScript/Aulas/ExerciciosJS.js
@@ -0,0 +1,10 @@
+let numero = Number(document.getElementById("numero").value);
+
+function dobro(){
+    let numero = Number(document.getElementById("numero").value);
+    const resultado = numero * 2;
+
+    document.getElementById("resultado").textContent = resultado;
+
+}
+
